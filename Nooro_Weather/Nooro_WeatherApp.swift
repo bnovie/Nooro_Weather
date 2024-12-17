a851ed6825f8930894f79eede49e44c2f894c503
bnovie <brian.novie@gmail.com>
2024-12-17 13:11:35 -0500

    Initial Commit

---
+++ Nooro_Weather/Nooro_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nooro_WeatherApp.swift
+//  Nooro_Weather
+//
+//  Created by Brian Novie on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Nooro_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
